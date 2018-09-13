ee36728f40083db5621dfd5c67a2714a99097096
Alexei Rodriguez <marodrig@alumni.purdue.edu>
2018-09-12 20:41:28 -0400

    chore: check-in work in progress.

--- battleships/battleship_api/views.py
+++ battleships/battleship_api/views.py
@@ -2,6 +2,8 @@
 Controllers for the Battleship application
 
 """
+import json
+
 from django.core.serializers import serialize
 from django.db import DatabaseError, DataError
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
@@ -9,7 +11,7 @@ from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
 from django.views.decorators.csrf import csrf_exempt
 
-from .models import Game,  Ship, ShipCoordinates
+from .models import Game, Ship, ShipCoordinates
 
 # Create your views here.
 
@@ -72,21 +74,35 @@ def get_game_coordinates(request, game_id):
     return HttpResponse(status=405)
 
 
-def get_game_ships(request, game_id):
+@csrf_exempt
+def get_post_game_ships(request, game_id):
     """
     """
+    game_inst = get_object_or_404(Game, pk=game_id)
     if request.method == 'GET':
-        game_inst = get_object_or_404(Game, pk=game_id)
         game_ships = game_inst.ship_set.all()
         return HttpResponse(serialize('json', game_ships))
-    return HttpResponse(status=405)
+    elif request.method == 'POST':
+        json_data = json.loads(request.body.decode('utf-8'))
+        random_placement = json_data.get('randomPlacement', False)
+        print(request.POST)
+        if random_placement:
+            game_inst.rand_init_ships()
+            ships_created = game_inst.ship_set.all().count()
+            data = {
+                'shipsCreated': ships_created,
+            }
+            return JsonResponse(data=data, status=201)
+        else:
+            return JsonResponse(data=request.POST)
+    else:
+        return HttpResponse(status=405)
 
 
 def get_post_ships(request):
     """
     """
-    if request.method == 'POST':
-        
+    pass
 
 
 def update_game_from_req(request):
@@ -103,10 +119,25 @@ def get_ships(request):
     if request.method == 'GET':
         all_ships = Ship.objects.all()
         return HttpResponse(serialize('json', all_ships))
-    elif request.method == 'POST':
-
     else:
         return HttpResponse(status=405)
+
+
+def get_ship_coordinates(request, ship_id):
+    """
+    """
+    if request.method == 'GET':
+        ship_inst = get_object_or_404(Ship, pk=ship_id)
+        ship_coordinates = ship_inst.shipcoordinates_set.all()
+        return HttpResponse(serialize('json', ship_coordinates))
+    else:
+        return HttpResponse(status=405)
+
+
+def get_ship(request, shipd_id):
+    """
+    """
+    return HttpResponse("Place holder, work in progress.")
 
 
 def get_ship_status(request, ship_id):
