1aeb829c10dff56cb47df1e4d632d116ae3b4c40
Alexei Rodriguez <marodrig@alumni.purdue.edu>
2018-09-13 19:24:57 -0400

    feat: adds serializers for classes using djangorest framework

---
+++ battleships/battleship_api/serializers.py
@@ -0,0 +1,50 @@
+"""
+Serializers for the battleship REST API
+
+"""
+from rest_framework import serializers
+from .models import Game, Ship, ShipCoordinates
+
+
+class GameSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Game Model
+
+    """
+    class Meta:
+        model = Game
+        fields = (
+            'id',
+            'is_over',
+            'start_date')
+
+
+class ShipSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Ship Model
+
+    """
+    class Meta:
+        model = Ship
+        fields = (
+            'id',
+            'game',
+            'orientation',
+            'length',
+            'is_alive')
+
+
+class ShipCoordinatesSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the ShipCoordinates Model
+
+    """
+    class Meta:
+        model = ShipCoordinates
+        fields = (
+            'id',
+            'ship',
+            'game',
+            'hit',
+            'row',
+            'column')
